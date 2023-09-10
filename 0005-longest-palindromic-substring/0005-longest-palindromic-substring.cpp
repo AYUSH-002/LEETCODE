f8cfe8296dff782975aa2e0e0c39cd1841d2ff0b
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-09-10 08:47:24 +0530

    Time: 16 ms (73.97%), Space: 16 MB (52.4%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+string solve(string &s,int i,int j)
+{
+    while(i>=0 && j<s.size() && s[i]==s[j])
+    {
+        i--;
+        j++;
+    }
+    return s.substr(i+1,j-i-1);
+}
+    string longestPalindrome(string s) 
+    {
+        ios::sync_with_stdio(0); cin.tie(0);
+        // Better Approach
+        int n=s.size();
+        string ans="";
+        for(int i=0;i<n;i++)
+        {
+            string temp=solve(s,i,i);
+            if(temp.size()>ans.size())
+            {
+                ans=temp;
+            }
+         temp=solve(s,i,i+1);
+            if(temp.size()>ans.size())
+            {
+                ans=temp;
+            }
+        }
+        return ans;
+        // // Normal Approach
+        // int n=s.size(),j;
+        // if(n==1)
+        // {
+        //     return s;
+        // }
+        // string sub="",subRev="",ans="";
+        // for(int i=0;i<n;i++)
+        // {
+        //     j=n-1;
+        //     while(i<j)
+        //     {
+        //         if(s[i]==s[j] && ans.size()<j-i+1)
+        //         {
+        //             sub=s.substr(i,j-i+1);
+        //             subRev=sub;
+        //             reverse(subRev.begin(),subRev.end());
+        //             if(sub==subRev)
+        //             {
+        //                 ans=sub;
+        //             }
+        //         }
+        //         j--;
+        //     }
+        // }
+        // if(ans.size()==0)
+        // {
+        //     ans=s[0];
+        // }
+        // return ans;
+    }
+};
