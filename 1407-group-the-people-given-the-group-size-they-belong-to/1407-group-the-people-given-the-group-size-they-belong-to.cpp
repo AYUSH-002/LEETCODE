5bdfde7e1898e6162898949eac30d34bbad717f2
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-09-11 16:33:41 +0530

    Time: 14 ms (35.21%), Space: 13.6 MB (28.92%) - LeetHub

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        vector<vector<int>>ans;
+        int n=groupSizes.size();
+        unordered_map<int,int>umap;
+        for(auto& x:groupSizes)
+        {
+            umap[x]++;
+        }
+        for(auto& x:umap)
+        {
+            int i=0;
+            vector<int>res;
+            while(res.size()<x.second && i<n)
+            {
+                if(x.first==groupSizes[i])
+                {
+                    res.push_back(i);
+                }
+                i++;
+            }
+            if(x.first!=x.second)
+            {
+                int p=x.second/x.first;
+                while(p>0)
+                {
+                    vector<int>a;
+                    while(a.size()<x.first)
+                    {
+                        a.push_back(res.back());
+                        res.pop_back();
+                    }
+                    ans.push_back(a);
+                    p--; 
+                }
+            }
+            else
+            {
+            ans.push_back(res);
+            }
+        }
+        return ans;
+    }
+};
