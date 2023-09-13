8d57e8f4e639f3708ac92f179ee65edad04ac213
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-09-13 16:49:39 +0530

    Time: 8 ms (98.27%), Space: 18.6 MB (9.92%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -4,27 +4,27 @@ public:
     {
         ios_base::sync_with_stdio(0); cin.tie(NULL);
         int n=ratings.size();
-        vector<int>dp(n,1);
+        vector<int>pre(n,1);
+        vector<int>post(n,1);
         for(int i=1;i<n;i++)
         {
             if(ratings[i]>ratings[i-1])
             {
-                dp[i]=dp[i-1]+1;
+                pre[i]=pre[i-1]+1;
             }
         }
         for(int i=n-2;i>=0;i--)
         {
-            if(ratings[i+1]<ratings[i] && dp[i]<=dp[i+1])
+            if(ratings[i+1]<ratings[i])
             {
-                dp[i]=dp[i+1]+1; 
+                post[i]=post[i+1]+1; 
             }
         }
         int res=0;
         for(int i=0;i<n;i++)
         {
-           res+=dp[i];
+           res+=max(pre[i],post[i]);
         }
         return res;
-
     }
 };
