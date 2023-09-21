4409c9cf4e255c0de7823b5ee3b789c85ba2bf4b
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-09-21 09:08:00 +0530

    Time: 31 ms (40.35%), Space: 90.5 MB (5.83%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    {
         ios::sync_with_stdio(0); cin.tie(0);
-        vector<int>ans;
-        int n=nums1.size();
-        int m=nums2.size();
-        double bns=0;
-        for(int i=0;i<n;i++)
+        vector<int>total;
+        for(auto& x:nums1)
         {
-            ans.push_back(nums1[i]);
+            total.push_back(x);
         }
-        for(int i=0;i<m;i++)
+        for(auto& x:nums2)
         {
-            ans.push_back(nums2[i]);
+            total.push_back(x);
         }
-        sort(ans.begin(),ans.end());
-        int p=n+m;
-        int a=0;
-        if(p%2==0)
+        sort(total.begin(),total.end());
+        int n=total.size();
+        if(n%2==0)
         {
-            a=p/2;
-            bns=(ans[a]+ans[a-1])/2.000;
+            return (total[n/2]+total[(n/2)-1])/2.000;    
         }
-        else
-        {
-            a=ceil(p/2);
-            bns=ans[a];
-        }
-        return bns;
+        return total[n/2];
     }
 };
