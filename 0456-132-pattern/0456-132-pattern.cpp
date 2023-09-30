7d5bb06c530c0c9c0e4e440f547a0cc5367523ca
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-09-30 09:01:06 +0530

    Time: 40 ms (99.08%), Space: 48.2 MB (83.56%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -3,7 +3,6 @@ public:
     bool find132pattern(vector<int>& nums) 
     {
         ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
-        /*what we are doing here is we are taking and empty stack and a min element then we are going from last to first in array and now we are checking if nums[i] less than min or not this will come into picture as we found the other two  now when while conditions matched we got our second element and we only want one less now so now if comes to picture*/
         int mini=INT_MIN;
         stack<int>s;
         int n=nums.size();
