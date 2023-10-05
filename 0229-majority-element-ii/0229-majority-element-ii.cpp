2b1869afc2db8df5106ac0db81acd1b789be3721
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-10-05 09:42:29 +0530

    Time: 16 ms (7.44%), Space: 16.3 MB (18.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) 
-    {
-        ios_base::sync_with_stdio(0); cin.tie(NULL);
-        vector<int>ans;
-        unordered_map<int,int>umap;
-        int n=nums.size();
-        for(auto& it:nums)umap[it]++;
-        for(auto& it:umap)
-        {
-            if(it.second>(n/3))
-            {
-                ans.push_back(it.first);
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int> umap;
+        int n = nums.size();
+        for(auto i:nums){
+            umap[i]++;
+        }
+
+        vector<int> res;
+        int x = floor(n/3);
+        for(auto i:umap){
+            if(i.second > x){
+                res.push_back(i.first);
             }
         }
-        return ans;
+
+        return res;
     }
 };
