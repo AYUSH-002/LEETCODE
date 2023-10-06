fac65e92952fa0a40a9c9ab4d941097935ca815d
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-10-06 11:47:45 +0530

    Time: 98 ms (100%), Space: 123.8 MB (27.92%) - LeetHub

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.cpp
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -1,19 +1,22 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
+
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
-        int n=nums.size();
-        vector<int>res=nums;
-        sort(res.begin(),res.end());
-        int i=n-1;
-        int j=(n-1)/2;
-        for(int k=0;k<n;k+=2)
-        {
-            nums[k]=res[j--];
-        }
-        for(int k=1;k<n;k+=2)
-        {
-            nums[k]=res[i--];
+        for (int i = 1; i < nums.size() - 1;) {
+            if (nums[i - 1] + nums[i + 1] == nums[i] + nums[i]) {
+                swap(nums[i], nums[i + 1]);
+                if (i > 1) {
+                    i--;
+                }
+            } else {
+                i++;
+            }
         }
         return nums;
     }
