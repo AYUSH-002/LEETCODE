928e53bcb73add25042e0362329992a618c0b4ce
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-10-06 11:04:56 +0530

    Time: 13 ms (76.48%), Space: 18.1 MB (40.91%) - LeetHub

---
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
+        int n=nums.size();
+        vector<int>res=nums;
+        sort(res.begin(),res.end());
+        int i=n-1;
+        int j=(n-1)/2;
+        for(int k=0;k<n;k+=2)
+        {
+            nums[k]=res[j--];
+        }
+        for(int k=1;k<n;k+=2)
+        {
+            nums[k]=res[i--];
+        }
+    }
+};
