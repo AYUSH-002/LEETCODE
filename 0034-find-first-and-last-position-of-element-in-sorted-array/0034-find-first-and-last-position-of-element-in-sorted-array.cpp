6657a67612c16d24ed20f0ba092c89131570729c
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-10-09 09:46:45 +0530

    Time: 4 ms (64.38%), Space: 14.2 MB (8.57%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
  public:
   vector<int> searchRange(vector<int>& nums, int target) {
+      ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
     const int low = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
     if (low== nums.size() || nums[low] != target)
       return {-1, -1};
