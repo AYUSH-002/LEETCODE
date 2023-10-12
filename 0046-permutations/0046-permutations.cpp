b873263834b2ee04cd36b7ca08875dc5d0773662
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-10-12 15:54:53 +0530

    Time: 0 ms (100%), Space: 7.9 MB (53.12%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,30 +1,18 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) 
     {
-        ios_base::sync_with_stdio(0); cin.tie(NULL);
-        int n=nums.size();
-        vector<vector<int>>res;
-        set<vector<int>>ans;
-        permutation(nums,ans,0,n-1);
-        for(auto& a:ans)
-        {
-            res.push_back(a);
-        }
+        vector<vector<int>> res; 
+        sort(nums.begin(), nums.end()); 
+        do 
+        res.push_back(nums); 
+        while (next_permutation(nums.begin(), nums.end())); 
         return res;
-    }
-    void permutation(vector<int>nums,set<vector<int>>&ans,int l,int r)
-    {
-        if(l>=nums.size())
-        {
-            ans.insert(nums);
-            return;
-        }
-        for(int i=l;i<=r;i++)
-        {
-            swap(nums[i],nums[l]);
-            permutation(nums,ans,l+1,r);
-            swap(nums[i],nums[l]);
-        }
     }
 };
