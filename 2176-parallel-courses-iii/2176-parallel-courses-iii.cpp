d4deadc734bbf9b3f3a20799d2220c6401de79ef
AYUSH SHARMA <100202279+AYUSH-002@users.noreply.github.com>
2023-10-18 14:17:52 +0530

    Time: 312 ms (76.08%), Space: 131.9 MB (71.58%) - LeetHub

---
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        vector<int> inDegree(n, 0);
+        vector<vector<int> > graph(n);
+
+        for (const vector<int>& relation : relations) {
+            int prevCourse = relation[0] - 1;
+            int nextCourse = relation[1] - 1;
+            graph[prevCourse].push_back(nextCourse);
+            inDegree[nextCourse]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        
+        vector<int> completionTime(n, 0);
+        int minTime = 0;
+        
+        while (!q.empty()) {
+            int course = q.front();
+            q.pop();
+            
+            for (int nextCourse : graph[course]) {
+                inDegree[nextCourse]--;
+                completionTime[nextCourse] = max(completionTime[nextCourse], completionTime[course] + time[course]);
+                
+                if (inDegree[nextCourse] == 0) {
+                    q.push(nextCourse);
+                }
+            }
+            
+            minTime = max(minTime, completionTime[course] + time[course]);
+        }
+        
+        return minTime;
+    }
+};
