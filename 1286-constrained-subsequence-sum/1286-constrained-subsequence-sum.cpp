f7440a70ebfeb52bbb9b2dc9f1d6bebce5297ddc
AYUSH SHARMA <022003ayush@gmail.com>
2023-10-21 10:33:39 +0530

    Time: 272 ms (52.86%), Space: 340.5 MB (5.1%) - LeetHub

---
+++ 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int constrainedSubsetSum(vector<int>& nums, int k) 
+    {
+        vector<int>dp(nums);
+        deque<int>res;
+        res.push_back(0);
+        for(int i=1;i<nums.size();i++)
+        {
+            while(!res.empty() && res.back()<i-k)
+            {
+                res.pop_back();
+            }
+            dp[i]=max(dp[i],dp[res.back()]+nums[i]);
+            while(!res.empty() && dp[res.front()]<=dp[i])
+            {
+                res.pop_front();
+            }
+            res.push_front(i);
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
