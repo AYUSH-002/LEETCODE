08c300cbb63d56bd3e2a02af283e6f1fd5ea0ee6
AYUSH SHARMA <022003ayush@gmail.com>
2023-10-25 10:16:49 +0530

    Time: 2 ms (51.37%), Space: 6.5 MB (12.22%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
@@ -1,16 +1,21 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        
-        if (n == 1) {
-        return 0;
-    }
-    int mid = pow(2,n-1)/2;
-    if (k <= mid) {
-        return kthGrammar(n - 1, k);
-    }
-    else {
-        return 1 - kthGrammar(n - 1, k - mid);
-    }
+        int res = 0;
+        while (n > 1) {
+            int mid = pow(2, n-2);
+            if (k > mid) {
+                k -= mid; 
+                res = 1 - res;
+            } 
+            n--;
+        }
+        return res;
     }
 };
