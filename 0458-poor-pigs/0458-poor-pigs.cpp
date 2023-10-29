6dea15eaa31a1e8e0dcdf4094c28b6c6739c1580
AYUSH SHARMA <022003ayush@gmail.com>
2023-10-29 21:30:58 +0530

    Time: 0 ms (100%), Space: 6.6 MB (25.93%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -0,0 +1,9 @@
+class Solution {
+ public:
+  int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+    int res = 0;
+        while((int)pow(minutesToTest / minutesToDie + 1, res) < buckets)
+            res ++;
+        return res;
+  }
+};
