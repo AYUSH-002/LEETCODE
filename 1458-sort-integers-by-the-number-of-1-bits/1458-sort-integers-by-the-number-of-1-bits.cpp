79b5389e866f63b38cdb1f9286c38463f196e5aa
AYUSH SHARMA <022003ayush@gmail.com>
2023-10-30 11:51:35 +0530

    Time: 10 ms (38.11%), Space: 10.4 MB (81%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,13 +1,19 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
 bool comp(int &a,int &b)
 {
-        int m=__builtin_popcountll(a);
-        int n=__builtin_popcountll(b);
-        if(m==n)
-        {
-            return a<b;
-        }
-        return m<n;
+    int m=__builtin_popcountll(a);
+    int n=__builtin_popcountll(b);
+    if(m==n)
+    {
+        return a<b;
     }
+    return m<n;
+}
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) 
