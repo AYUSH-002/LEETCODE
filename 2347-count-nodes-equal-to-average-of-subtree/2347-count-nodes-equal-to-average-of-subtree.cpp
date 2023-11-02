7caa75c0398ca8b9bf12640f5a2d4d7fd5d64bf1
AYUSH SHARMA <022003ayush@gmail.com>
2023-11-02 09:15:05 +0530

    Time: 4 ms (80.67%), Space: 12 MB (91.42%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
@@ -17,26 +17,51 @@
 }();
 class Solution {
 public:
-    int countNodes(TreeNode* root)
+     pair<int,int> solve(TreeNode* root , int &res)
     {
-        if(root==NULL) return 0;
-        return 1+countNodes(root->left)+countNodes(root->right);
+        if(root==NULL)
+        return make_pair(0,0);
+
+         pair<int,int>low=solve(root->left,res);
+         pair<int,int>high=solve(root->right,res);
+
+         int sum=low.first+high.first+root->val;
+        int  nodes=low.second+high.second+1;
+         if(sum/nodes==root->val)
+          res++;
+
+        return make_pair(sum, nodes);
+
     }
-    int sumNodes(TreeNode* root)
-    {
-        if(root==NULL) return 0;
-        return root->val+sumNodes(root->left)+sumNodes(root->right);
-    }
-    int averageOfSubtree(TreeNode* root) 
-    {
-        if(root==NULL) return 0;
-        int sum=sumNodes(root);
-        int nodes=countNodes(root);;
-        int avg=round(floor(sum/nodes));
-        if(avg==root->val)
-        {
-            return 1+averageOfSubtree(root->left)+averageOfSubtree(root->right);
-        }
-        return averageOfSubtree(root->left)+averageOfSubtree(root->right);
+   
+    int averageOfSubtree(TreeNode* root) {
+        int res=0;
+        solve(root,res);
+        return res;
     }
 };
+// class Solution {
+// public:
+//     int countNodes(TreeNode* root)
+//     {
+//         if(root==NULL) return 0;
+//         return 1+countNodes(root->left)+countNodes(root->right);
+//     }
+//     int sumNodes(TreeNode* root)
+//     {
+//         if(root==NULL) return 0;
+//         return root->val+sumNodes(root->left)+sumNodes(root->right);
+//     }
+//     int averageOfSubtree(TreeNode* root) 
+//     {
+//         if(root==NULL) return 0;
+//         int sum=sumNodes(root);
+//         int nodes=countNodes(root);;
+//         int avg=round(floor(sum/nodes));
+//         if(avg==root->val)
+//         {
+//             return 1+averageOfSubtree(root->left)+averageOfSubtree(root->right);
+//         }
+//         return averageOfSubtree(root->left)+averageOfSubtree(root->right);
+//     }
+// };
