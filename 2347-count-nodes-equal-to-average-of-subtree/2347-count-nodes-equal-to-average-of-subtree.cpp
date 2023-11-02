7af4cabfae64e39c9c9dfba528f030303db30b82
AYUSH SHARMA <022003ayush@gmail.com>
2023-11-02 09:12:31 +0530

    Time: 28 ms (11.93%), Space: 12.1 MB (82.45%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
@@ -9,6 +9,12 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
 class Solution {
 public:
     int countNodes(TreeNode* root)
@@ -25,9 +31,7 @@ public:
     {
         if(root==NULL) return 0;
         int sum=sumNodes(root);
-        int nodes=countNodes(root);
-        // int sum=root->val+sumNodes(root->left)+sumNodes(root->right); 
-        // int nodes=1+countNodes(root->left)+countNodes(root->right);
+        int nodes=countNodes(root);;
         int avg=round(floor(sum/nodes));
         if(avg==root->val)
         {
