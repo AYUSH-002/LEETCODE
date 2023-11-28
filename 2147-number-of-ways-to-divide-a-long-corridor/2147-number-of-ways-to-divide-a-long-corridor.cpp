ca7620648adaaf1db4a31e5b16da06d82ded94f8
AYUSH SHARMA <022003ayush@gmail.com>
2023-11-28 18:21:46 +0530

    Time: 90 ms (100.00%), Space: 56 MB (33.71%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,27 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
+class Solution {
+public:
+    int numberOfWays(string corridor) {
+        vector<int> s;
+        for (int i = 0; i < corridor.size(); ++i) {
+            if (corridor[i] == 'S')
+                s.push_back(i);
+        }
+
+        if (s.empty() || (s.size() & 1)) 
+            return 0;
+
+        int mod = 1e9 + 7;
+        long long ans = 1;
+        for (int i = 1; i < s.size() - 2; i += 2) {
+            ans = ans * (s[i + 1] - s[i]) % mod;
+        }
+
+        return ans;
+    }
+};
