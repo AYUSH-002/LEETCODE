530cbbdb5240bb33e72c6ef330988f0a7472d2ed
AYUSH SHARMA <022003ayush@gmail.com>
2023-12-16 08:57:52 +0530

    Time: 13 ms (24.13%), Space: 7.5 MB (98.01%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,25 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s.size()!=t.size())
+        {
+            return false;
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]!=t[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
