9705ebcbf65f2802f2b964c0f28244b7a4b20369
AYUSH SHARMA <022003ayush@gmail.com>
2024-01-04 12:07:00 +0530

    Time: 3 ms (58.20%), Space: 6.9 MB (11.17%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        int ans=-1;
-        for(int i=0;i<s.size();i++){
-            for(int j=i+1;j<s.size();j++){
-                if(s[i]==s[j]){
-                    ans=max(ans,j-i-1);
-                }
+        unordered_map<char,int>umap;
+        int res=-1;
+        for(int i=0;i<s.size();i++)
+        {
+            if(umap.find(s[i])==umap.end())
+            {
+                umap[s[i]]=i;
+            }
+            else
+            {
+                res=max(res,i-umap[s[i]]-1);
             }
         }
-        return ans;
+        return res;
     }
 };
