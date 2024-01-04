69b7c7098039092fee814d897a424b297fc5dba9
AYUSH SHARMA <022003ayush@gmail.com>
2024-01-04 11:47:32 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (11.52%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     bool isPathCrossing(string path) {
-        int x = 0;
-        int y = 0;
-        set<pair<int,int>>  st;
-        st.insert({0,0});
-        for(auto t: path){
-            if(t=='N') y++;
-            else if(t=='S') y--;
-            else if(t=='E') x++;
-            else x--;
-            if(st.find({x,y}) == st.end()) {
-                st.insert({x,y});
-            }else return true;
-        }
-        return false;
+       int x=0;
+       int y=0;
+       int n=path.size();
+       set<string>visited;
+       visited.insert("0,0");
+       for(int i=0;i<n;i++)
+       {
+           if(path[i]=='N') y++;
+           else if(path[i]=='S')y--;
+           else if(path[i]=='E')x++;
+           else if(path[i]=='W')x--;
+           string res=to_string(x)+","+to_string(y);
+           if(visited.find(res)!=visited.end())
+           {
+               return true;
+           }
+           visited.insert(res);
+       }
+       return false;
     }
 };
