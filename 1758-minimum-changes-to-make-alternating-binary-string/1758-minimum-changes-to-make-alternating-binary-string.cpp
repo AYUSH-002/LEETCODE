271a3bbfcda220882c63a60ede01267ce142d89a
AYUSH SHARMA <022003ayush@gmail.com>
2024-01-14 20:30:51 +0530

    Time: 3 ms (82.49%), Space: 7.8 MB (8.17%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int minOperations(string s) {
-        auto count_changes = [](char start_char, const string& str) {
-        int changes = 0;
-        for (char c : str) {
-            if (c != start_char) {
-                changes++;
+    int solution(char c,string s)
+    {
+        int sol=0;
+        for(char x:s)
+        {
+            if(x!=c)
+            {
+                sol++;
             }
-            start_char = (start_char == '0') ? '1' : '0';
+            c=(c=='0')?'1':'0';
         }
-        return changes;
-    };
-
-    int changes_starting_zero = count_changes('0', s);
-    int changes_starting_one = count_changes('1', s);
-
-    return min(changes_starting_zero, changes_starting_one);
+        return sol;
+    }
+    int minOperations(string s) {
+        return min(solution('0',s),solution('1',s));
     }
 };
