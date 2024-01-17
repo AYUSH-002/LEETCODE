ad905d0d529e52b3280ae0b4a7ad1bf380837869
AYUSH SHARMA <022003ayush@gmail.com>
2024-01-17 14:27:07 +0530

    Time: 5 ms (39.16%), Space: 8.8 MB (20.45%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -4,27 +4,45 @@ static int speedup = []() {
 	cout.tie(0);
 	return 0;
 }();
+// class Solution {
+// public:
+//     bool uniqueOccurrences(vector<int>& arr) {
+//         map<int,int>umap;
+//         for(auto x:arr)
+//         {
+//             umap[x]++;
+//         }
+//         vector<int>res;
+//         for(auto x:umap)
+//         {
+//             res.push_back(x.second);
+//         } 
+//         sort(res.begin(),res.end());
+//         for(int i=0;i<res.size()-1;i++)
+//         {
+//             if(res[i]==res[i+1])
+//             {
+//                 return false;
+//             }
+//         } 
+//         return true;
+//     }
+// };
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        map<int,int>umap;
-        for(auto x:arr)
-        {
-            umap[x]++;
+        unordered_map<int, int> m;
+        for(int i=0; i<arr.size(); i++){
+            m[arr[i]]++;
         }
-        vector<int>res;
-        for(auto x:umap)
-        {
-            res.push_back(x.second);
-        } 
-        sort(res.begin(),res.end());
-        for(int i=0;i<res.size()-1;i++)
-        {
-            if(res[i]==res[i+1])
-            {
-                return false;
+        unordered_set<int> s;
+        for(auto x : m ){
+            int freq = x.second;
+            if(s.find(freq) != s.end()) return false;
+            else{
+                s.insert(freq);
             }
-        } 
+        }
         return true;
     }
 };
