c380a855b734bedbb824e10f78d91140b0450364
AYUSH SHARMA <022003ayush@gmail.com>
2024-01-17 14:28:19 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (41.08%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -4,30 +4,6 @@ static int speedup = []() {
 	cout.tie(0);
 	return 0;
 }();
-// class Solution {
-// public:
-//     bool uniqueOccurrences(vector<int>& arr) {
-//         map<int,int>umap;
-//         for(auto x:arr)
-//         {
-//             umap[x]++;
-//         }
-//         vector<int>res;
-//         for(auto x:umap)
-//         {
-//             res.push_back(x.second);
-//         } 
-//         sort(res.begin(),res.end());
-//         for(int i=0;i<res.size()-1;i++)
-//         {
-//             if(res[i]==res[i+1])
-//             {
-//                 return false;
-//             }
-//         } 
-//         return true;
-//     }
-// };
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
