11fd9d2dc882639c5ddb6f41580ef58f9e4826a2
AYUSH SHARMA <022003ayush@gmail.com>
2024-01-19 18:31:13 +0530

    Time: 3 ms (98.31%), Space: 20.8 MB (99.77%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -1,26 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
 class Solution {
- public:
-  vector<int> largestValues(TreeNode* root) {
-    if (root == nullptr)
-      return {};
-
-    vector<int> ans;
-    queue<TreeNode*> q{{root}};
-
-    while (!q.empty()) {
-      int maxi = INT_MIN;
-      for (int sz = q.size(); sz > 0; --sz) {
-        TreeNode* node = q.front();
-        q.pop();
-        maxi = max(maxi, node->val);
-        if (node->left)
-          q.push(node->left);
-        if (node->right)
-          q.push(node->right);
-      }
-      ans.push_back(maxi);
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int>res;
+        if(root==nullptr)
+        {
+            return res;
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n=q.size();
+            int maxi=INT_MIN;
+            for(int i=0;i<n;i++)
+            {
+                TreeNode *temp=q.front();
+                q.pop();
+                if(temp->val>maxi)
+                {
+                    maxi=temp->val;
+                }
+                if(temp->left)
+                {
+                    q.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
+            res.push_back(maxi);
+            
+        }
+        return res;
     }
-
-    return ans;
-  }
 };
