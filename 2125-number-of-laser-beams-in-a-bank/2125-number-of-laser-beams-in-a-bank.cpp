ea0a2029b82930bbec41945b01950070cff20262
AYUSH SHARMA <022003ayush@gmail.com>
2024-01-29 16:36:56 +0530

    Time: 43 ms (98.89%), Space: 26.7 MB (30.75%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,21 +1,35 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int lastSum = 0, currSum;
-        int totalBeams = 0;
-        for(auto &row : bank){
-            currSum = getOneCount(row);
-            totalBeams += (currSum * lastSum);
-            lastSum = currSum>0? currSum : lastSum;
+        vector<int>res;
+        int n=bank.size();
+        int m=bank[0].size();
+        for(int i=0;i<n;i++)
+        {
+            int count=0;
+            for(int j=0;j<m;j++)
+            {
+                if(bank[i][j]=='1')
+                {
+                    count++;
+                }
+            }
+            if(count>0)
+            {
+                res.push_back(count);
+            }
         }
-        return totalBeams;
-    }
-
-    int getOneCount(string &s){
-        int count = 0;
-        for(auto &ch : s){
-            count += ch=='1';
+        int sol=0;
+        for(int i=1;i<res.size();i++)
+        {
+            sol+=(res[i-1]*res[i]);
         }
-        return count;
+        return sol;
     }
 };
