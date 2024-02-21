4f568a1dc08bf1565c66878053aaebe91928db7f
AYUSH SHARMA <022003ayush@gmail.com>
2024-02-21 22:56:09 +0530

    Time: 4 ms (31.89%), Space: 7.9 MB (28.36%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,23 +1,28 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
 class Solution {
 public:
-    vector<vector<int>> generate(int n) 
-    {
-        ios::sync_with_stdio(0); cin.tie(0);
-        vector<vector<int>>ans;
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>>res;
         for(int i=0;i<n;i++)
-        {   
-            ans.push_back(vector<int>(i+1,1));
+        {
+            res.push_back(vector<int>(i+1,1));
         }
         for(int i=2;i<n;i++)
         {
-            for(int j=1;j<n;j++)
+            int m=res[i].size();
+            for(int j=1;j<m;j++)
             {
                 if(j<i)
                 {
-                    ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
+                    res[i][j]=res[i-1][j-1]+res[i-1][j];
                 }
             }
         }
-        return ans;
+        return res;
     }
 };
