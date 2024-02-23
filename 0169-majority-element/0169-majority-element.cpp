885511ffd68862c468dd57ee9efdd61d08048c6c
AYUSH SHARMA <022003ayush@gmail.com>
2024-02-23 23:13:36 +0530

    Time: 15 ms (49.15%), Space: 27.8 MB (5.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,9 +3,8 @@ public:
     int majorityElement(vector<int>& nums) 
     {
         ios_base::sync_with_stdio(0); cin.tie(NULL);
-        map<int,int>mp;
-        for (auto &it:nums) mp[it]++;
-        for (auto &it:mp) if(it.second>nums.size()/2) return it.first;
-        return 1;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        return nums[n/2];
     }
 };
