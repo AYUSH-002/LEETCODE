bd7ba2a2b57d462240ca786ab9ca0758b64a9844
AYUSH SHARMA <022003ayush@gmail.com>
2024-02-24 18:04:51 +0530

    Time: 81 ms (77.56%), Space: 73.6 MB (45.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -7,27 +7,49 @@ static int speedup = []() {
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+
         int n=nums.size();
         if(n==0 || n==1) return n;
-        int count1=0;
-        int count2=INT_MIN;
-        map<int,int>mp;
+        int res=1;
+        unordered_set<int>ans;
         for(auto num:nums)
         {
-            mp[num]++;
+            ans.insert(num);
         }
-        auto itr=mp.begin();
-        while(itr!=prev(mp.end()))
+        for(auto it:ans)
         {
-            int temp=(next(itr)->first-(itr)->first);
-            if(temp==1) count1++;
-            else
+            if(ans.find(it-1)==ans.end())
             {
-                count2=max(count2,count1);
-                count1=0;
+                int temp=it;
+                int currentLong=1;
+                while(ans.find(temp+1)!=ans.end())
+                {
+                    temp++;
+                    currentLong++;
+                }
+                res=max(currentLong,res);
             }
-            ++itr;
         }
-        return max(count1,count2)+1;
+        return res;
+        // int count1=0;
+        // int count2=INT_MIN;
+        // map<int,int>mp;
+        // for(auto num:nums)
+        // {
+        //     mp[num]++;
+        // }
+        // auto itr=mp.begin();
+        // while(itr!=prev(mp.end()))
+        // {
+        //     int temp=(next(itr)->first-(itr)->first);
+        //     if(temp==1) count1++;
+        //     else
+        //     {
+        //         count2=max(count2,count1);
+        //         count1=0;
+        //     }
+        //     ++itr;
+        // }
+        // return max(count1,count2)+1;
     }
 };
