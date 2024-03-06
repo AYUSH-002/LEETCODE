e695fd8c1c8648007cd1944298aecc66dac11b45
AYUSH SHARMA <022003ayush@gmail.com>
2024-03-06 23:56:06 +0530

    Time: 5 ms (61.23%), Space: 16.2 MB (9.63%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -7,5 +7,70 @@ class Solution {
       return {-1, -1};
     const int high = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
     return {low, high};
-  }
-};
+  }};
+// };class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         int n=nums.size();
+//         vector<int>res;
+//         int low=0;
+//         int high=n-1;
+//         if(n==1)
+//         {
+//             if(nums[0]==target)
+//             {
+//                 res.push_back(0);
+//                 res.push_back(0);
+//             }
+//             else
+//             {
+//                 res.push_back(-1);
+//                 res.push_back(-1);
+//             }
+//         }
+//         while(low<=high)
+//         {
+//             if(nums[low]==target)
+//             {
+//                 res.push_back(low);
+//                 low++;
+//                 continue;
+//             }
+//             if(nums[high]==target)
+//             {
+//                 res.push_back(high);
+//                 high--;
+//                 continue;
+//             }  
+//             if(nums[low]<target)
+//             {
+//                 low++;
+//             }
+//             if(nums[high]>target)
+//             {
+//                 high--;
+//             }
+            
+//         }
+//         if(res.size()==0)
+//         {
+//             res.push_back(-1);
+//             res.push_back(-1);
+//         }
+//         if(res.size()==1)
+//         {
+//             res.push_back(res[0]);
+//         }
+//         int a=res[res.size()-1];
+//         while(res.size()!=1)
+//         {
+//             res.pop_back();
+//         }
+//         res.push_back(a);
+//         if(res[0]>res[1])
+//         {
+//             swap(res[0],res[1]);
+//         }
+//         return res;
+//     }
+// };
