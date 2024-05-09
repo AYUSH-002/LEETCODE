7d305084acdb6480675888c5850239181116cc7a
AYUSH SHARMA <022003ayush@gmail.com>
2024-05-10 01:16:44 +0530

    Time: 162 ms (55.65%), Space: 107.2 MB (59.71%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end(), greater<int>());
+        long long ans = 0;
+        for(int i = 0; i < k; ++i) 
+            ans += max(happiness[i] - i, 0);
+        return ans;
+    }
+};
