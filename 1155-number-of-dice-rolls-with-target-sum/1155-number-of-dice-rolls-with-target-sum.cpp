347bf55a3f31baf485125a96e10a7a88d3da7ea6
AYUSH SHARMA <022003ayush@gmail.com>
2024-06-01 12:09:39 +0530

    Time: 17 ms (52.31%), Space: 9.8 MB (24.22%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,19 +1,29 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
 class Solution {
-public:
+public: 
     int mod = int(1e9 + 7);
-    int solve(int curr, int n,int k, int target, int sum,vector<vector<int>> &dp) {
-        if(sum > target) return 0;
-        if(sum == target && curr == n+1) return 1;
-        if(curr > n) return 0;
-        if(dp[curr][sum] != -1) return dp[curr][sum];
 
-        int temp = 0;
-        for(int i=1; i<=k; i++) {
-            if(sum+i <= target) 
-                temp = (temp + solve(curr+1, n,k,target,sum+i,dp)) % mod;
+    int solve(int curr,int n,int k,int target,int sum,vector<vector<int>>&dp)
+    {
+        if(sum>target) return 0;
+        if(sum==target && curr==n+1) return 1;
+        if(curr>n) return 0;
+        if(dp[curr][sum]!=-1) return dp[curr][sum];
+        int newSum=0;
+        for(int i=1;i<=k;i++)
+        {
+            if(sum+i<=target)
+            {
+                newSum=(newSum+solve(curr+1,n,k,target,sum+i,dp))%mod;
+            }
             else break;
         }
-        return dp[curr][sum] = temp;
+        return dp[curr][sum]=newSum;
     }
     int numRollsToTarget(int n, int k, int target) {
         vector<vector<int>> dp(n+1, vector<int>(target+1,-1));
