8691701e81d264aef7d19bdaacf7578fdd4d5950
AYUSH SHARMA <022003ayush@gmail.com>
2024-06-01 10:59:11 +0530

    Time: 196 ms (27.97%), Space: 41.1 MB (34.16%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,31 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
+class Solution {
+ public:
+  int longestStrChain(vector<string>& words) {
+    if (words.empty()) return 0;
+    sort(words.begin(), words.end(), [](string a, string b)
+    {return a.size() < b.size();});
+    int result=1;
+    map<string,int>mp;
+    for(const string &word:words)
+    {
+        mp[word]=1;
+        for(int j=0;j<word.size();j++)
+        {
+            string temp=word;
+            temp.erase(j,1);
+            if(!word.empty() && mp.find(temp)!=mp.end())
+            {
+                mp[word]=max(mp[word],mp[temp]+1);
+            }
+            result=max(result,mp[word]);
+        }
+    }
+    return result;
+  }
+};
