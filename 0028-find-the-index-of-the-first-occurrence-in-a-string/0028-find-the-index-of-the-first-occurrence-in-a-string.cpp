7506e8b2c1b779f35ce1a9f74e49a0ebddda496a
AYUSH SHARMA <022003ayush@gmail.com>
2024-06-06 10:48:16 +0530

    Time: 0 ms (100%), Space: 7.8 MB (22.13%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,45 +1,19 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        ios_base::sync_with_stdio(0); cin.tie(NULL);
         int n=haystack.size();
         int m=needle.size();
-        if(n==m && n==1 && haystack[0]==needle[0])
-        {
-            return 0;
-        }
-        if(n==m && n>1)
-        {
-            for(int i=0;i<n;i++)
-            {
-                if(haystack[i]!=needle[i]){
-                    return -1;
-                }
-            }
-            return 0;
-        }
-        else if(m>n)
-        {
-            return -1;
-        }
-        else
-        {
         for(int i=0;i<=n-m;i++)
         {
-            int j;
-            for(j=0;j<m;j++)
-            {
-                if(haystack[i+j]!=needle[j])
-                {
-                    break;
-                }
-            }
-            if(j==m)
-            {
-                return i;
-            }
+            string temp=haystack.substr(i,m);
+            if(temp==needle) return i;
         }
         return -1;
-        }
     }
 };
