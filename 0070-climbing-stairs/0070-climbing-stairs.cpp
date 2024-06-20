bfdef5fe8f4c21fd66ff05d1967217ae9cafe8db
AYUSH SHARMA <022003ayush@gmail.com>
2024-06-20 13:03:05 +0530

    Time: 0 ms (100%), Space: 7.5 MB (11.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,9 +6,8 @@ static int speedup = []() {
 }();
 class Solution {
 public:
-    int climbStairs(int n) 
-    {
-        vector<int>dp(n+1);
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
         dp[0]=1;
         dp[1]=1;
         for(int i=2;i<=n;i++)
