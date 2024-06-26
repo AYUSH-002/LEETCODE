387e9f0df20825fa626c68093068ae26a3783392
AYUSH SHARMA <022003ayush@gmail.com>
2024-06-26 15:20:37 +0530

    Time: 10 ms (99.32%), Space: 16.1 MB (62.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -6,20 +6,19 @@ static int speedup = []() {
 }();
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int n) 
-    {
-        vector<int>dp(n+1,INT_MAX);
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>dp(amount+1,INT_MAX);
         dp[0]=0;
-        for(int coin:coins)
+        for(const int coin:coins)
         {
-            for(int i=coin;i<=n;i++)
+            for(int i=coin;i<=amount;i++)
             {
-                if(i - coin >= 0 && dp[i - coin] != INT_MAX)
+                if(dp[i-coin]!=INT_MAX)
                 {
-                dp[i]=min(dp[i],dp[i-coin]+1);
+                    dp[i]=min(dp[i],dp[i-coin]+1);
                 }
             }
         }
-        return (dp[n]==INT_MAX) ? -1: dp[n];        
+        return dp[amount]==INT_MAX ? -1: dp[amount];
     }
 };
