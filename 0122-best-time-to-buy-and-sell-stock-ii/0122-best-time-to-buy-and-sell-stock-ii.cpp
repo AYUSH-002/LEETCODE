789cf383db360f0455531f282781d282aa10b063
AYUSH SHARMA <022003ayush@gmail.com>
2024-07-02 16:31:32 +0530

    Time: 0 ms (100%), Space: 15.6 MB (37.64%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,23 +4,38 @@ static int speedup = []() {
 	cout.tie(0);
 	return 0;
 }();
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n=prices.size();
+//         int buy=prices[0];
+//         int sell=0;
+//         for(int i=1;i<n;i++)
+//         {
+//             if(prices[i-1]<prices[i])
+//             {
+//                 sell+=(prices[i]-prices[i-1]);
+//                 buy=prices[i-1];
+//             }
+//         }
+//         return sell;
+//     }
+// };
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) 
-    {
-        int n=prices.size();
-        int sell=0;
-        int buy=prices[0];
-        for(int i=1;i<n;i++)
-        {
-            if(prices[i-1]>prices[i])
-            {
-                sell+=(prices[i-1]-buy);
-                buy=prices[i];
-            }
-            cout<<sell<<" "<<buy<<endl;
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n <= 1) return 0;
+        
+        vector<int> dp(2, 0);
+        
+        dp[1] = -prices[0]; 
+        
+        for (int i = 1; i < n; ++i) {
+            dp[0] = max(dp[0], dp[1] + prices[i]); 
+            dp[1] = max(dp[1], dp[0] - prices[i]);
         }
-        sell+=prices[n-1]-buy;
-        return sell;
+        
+        return dp[0]; 
     }
 };
