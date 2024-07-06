0c556367d1a0afaea80efa9efb64d6e1a9d11ecf
AYUSH SHARMA <022003ayush@gmail.com>
2024-07-06 09:46:44 +0530

    Time: 64 ms (71.19%), Space: 12.8 MB (67.92%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,64 @@
+// static int speedup = []() {
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(nullptr);
+// 	cout.tie(0);
+// 	return 0;
+// }();
+// class Solution {
+// public:
+//     int solve(int i,vector<int>& nums,vector<int>& dp)
+//     {
+//         int n=nums.size();
+//         if(i==n) return 0;
+//         if(dp[i]!=-1) return dp[i];
+//         int count=1;
+//         for(int j=i;j<n;j++)
+//         {
+//             if(nums[i]<nums[j])
+//             {
+//                 count=max(count,1+solve(j,nums,dp));
+//             }
+//         }
+//         return dp[i]=count;
+//     }
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         int maxLength = 0;
+//         vector<int> dp(n, -1);
+//         for (int i = 0; i < n; i++) {
+//             maxLength=max(maxLength,solve(i,nums,dp));
+//         }
+//         return maxLength;
+//     }
+// };
+static int speedup = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(0);
+    return 0;
+}();
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>v;
-        v.push_back(nums[0]);
-        int ans=1;
-        int n=nums.size();
-        for(int i=1;i<n;i++)
-        {
-            if(nums[i]>v.back())
-            {
-                  v.push_back(nums[i]);
-                  ans++;
-            }
-            else
-            {
-                int j = lower_bound(v.begin(),v.end(),nums[i]) - v.begin();
-                v[j]=nums[i];
+        int n = nums.size();
+        if (n == 0) return 0;
+
+        vector<int> dp(n, 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
             }
         }
-        return ans;
+
+        return *max_element(dp.begin(), dp.end());
     }
 };
+
+
+
+
+
