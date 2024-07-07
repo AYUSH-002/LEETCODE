158043189c57d2f53d2a147ad112a92246185691
AYUSH SHARMA <022003ayush@gmail.com>
2024-07-07 09:22:28 +0530

    Time: 0 ms (100%), Space: 7.1 MB (30.43%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,9 +1,3 @@
-static int speedup = []() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(0);
-	return 0;
-}();
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
