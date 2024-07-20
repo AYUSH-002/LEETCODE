aa78330d195c8836ffc22417dec241242a1baa0b
AYUSH SHARMA <022003ayush@gmail.com>
2024-07-20 11:02:05 +0530

    Time: 0 ms (100%), Space: 7.7 MB (14.92%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,7 +1,12 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
 class Solution {
 public:
-    bool isSubsequence(string s,string t) 
-    {
+    bool isSubsequence(string s, string t) {
         int n=s.size();
         int m=t.size();
         int i=0,j=0;
