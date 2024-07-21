c359c9a4533ebf073e9c0d8c37c25602b0b8fdf1
AYUSH SHARMA <022003ayush@gmail.com>
2024-07-21 23:21:41 +0530

    Time: 0 ms (100%), Space: 11.7 MB (6.67%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,48 @@
+static int speedup = []() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(0);
+	return 0;
+}();
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> m;
+        for(int i=0; i<arr.size(); i++){
+            m[arr[i]]++;
+        }
+        unordered_set<int> s;
+        for(auto x : m ){
+            int freq = x.second;
+            if(s.find(freq) != s.end()) return false;
+            else{
+                s.insert(freq);
+            }
+        }
+        return true;
+    }
+};
+// class Solution {
+// public:
+//     bool uniqueOccurrences(vector<int>& arr) {
+//         map<int,int>umap;
+//         for(auto x:arr)
+//         {
+//             umap[x]++;
+//         }
+//         vector<int>res;
+//         for(auto x:umap)
+//         {
+//             res.push_back(x.second);
+//         } 
+//         sort(res.begin(),res.end());
+//         for(int i=0;i<res.size()-1;i++)
+//         {
+//             if(res[i]==res[i+1])
+//             {
+//                 return false;
+//             }
+//         } 
+//         return true;
+//     }
+// };
