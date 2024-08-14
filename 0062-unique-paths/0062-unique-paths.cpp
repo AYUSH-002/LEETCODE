0620208fec2d853e771221f71ecc16b804accd32
AYUSH SHARMA <022003ayush@gmail.com>
2024-08-14 11:31:45 +0530

    Time: 0 ms (100%), Space: 7.5 MB (80.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -6,17 +6,19 @@ static int speedup = []() {
 }();
 class Solution {
 public:
-    int uniquePaths(int m, int n) 
-    {
+    int uniquePaths(int m, int n) {
         int dp[m][n];
+
         for(int i=0;i<m;i++)
         {
             dp[i][0]=1;
         }
-        for(int j=0;j<n;j++)
+
+        for(int i=0;i<n;i++)
         {
-            dp[0][j]=1;
+            dp[0][i]=1;
         }
+        
         for(int i=1;i<m;i++)
         {
             for(int j=1;j<n;j++)
